61826c81752682894a37c87fed51fb06af81585f
Rashaad Akbar <rushda2000@gmail.com>
2022-07-27 12:11:25 +0530

    update User interface

--- src/frontend/src/lib/store.ts
+++ src/frontend/src/lib/store.ts
@@ -1,18 +1,23 @@
-
 import { writable } from 'svelte/store';
 
+export interface UserStats {
+	confusion_chess: { wins: number; losses: number; draws: number };
+	magic_chess: { wins: number; losses: number; draws: number };
+}
+
 export interface User {
-    id: number;
-    username: string;
-    is_staff: boolean;
+	id: number;
+	username: string;
+	is_staff: boolean;
+	is_playing: boolean;
+	stats: UserStats;
 }
 
 export const userStore = writable<User | null>(null);
 
-
 export interface Appearance {
-    board_style: string;
-    piece_set: string;
+	board_style: string;
+	piece_set: string;
 }
 
 export const appearanceStore = writable<Appearance | null>(null);
