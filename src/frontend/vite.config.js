e80139a21c024f931c248c887ead2dbd4ec9077b
Rashaad Akbar <rushda2000@gmail.com>
2022-07-28 20:58:38 +0530

    proxy websocket

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -5,17 +5,19 @@ const config = {
 	plugins: [sveltekit()],
 	server: {
 		proxy: {
-		  '/api/': {
-			target: 'http://127.0.0.1:8000/'
-		  }
-		},
+			'/api/ws/play/': {
+				// WebSocket Proxy
+				target: 'http://127.0.0.1:8000/',
+				ws: true
+			},
+			'/api/': {
+				target: 'http://127.0.0.1:8000/'
+			}
+		}
 	},
 	ssr: {
-		noExternal: [
-			'svelte-use-chessground',
-			'chessground'
-		]
-	},
+		noExternal: ['svelte-use-chessground', 'chessground']
+	}
 };
 
 export default config;
