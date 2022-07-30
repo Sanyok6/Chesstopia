a4335a90e9d83fbf6858cc390644d89fb0209e7f
Sanyok6 <tishchenkoa2008@gmail.com>
2022-07-29 23:24:55 -0400

    fen is now generated by backend

--- src/backend/chess/models.py
+++ src/backend/chess/models.py
@@ -2,10 +2,22 @@ from django.conf import settings
 from django.db import models
 from django.utils.crypto import get_random_string
 
+import random
 
 def generate_id():
     return get_random_string(20)
 
+def generate_fen():
+    pieces = ["r", "n", "b", "q"]
+    def get_random_layout():
+        layout = ["k"]
+        for p in range(7): 
+            layout.append(pieces[random.randint(0, len(pieces) - 1)])
+        random.shuffle(layout)
+        return str(layout).strip('[]').replace('\'', '').replace(', ', '')
+
+    fen = get_random_layout()+"/pppppppp/8/8/8/8/PPPPPPPP/"+get_random_layout().upper()+" w KQkq - 0 1"
+    return fen
 
 class ChessMatch(models.Model):
     CHESS_MATCH_TYPES = [
@@ -18,6 +30,6 @@ class ChessMatch(models.Model):
     black = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL,
                               related_name="chess_match_black")
     type = models.CharField(max_length=50, choices=CHESS_MATCH_TYPES)
-    starting_pos = models.TextField(default="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    starting_pos = models.TextField(default=generate_fen)
     created_at = models.DateTimeField(auto_now_add=True)
     result = models.IntegerField(choices=((0, "Draw"), (1, "White wins"), (-1, "Black wins")), null=True, blank=True)
